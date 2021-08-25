42a64db68dfbebc7d33e76572a08b58ddb4853bd
Mu Idrees <idris@y7mail.com>
2021-08-25 14:24:24 +0930

    basic components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TasksComponent, TaskItemComponent, ListsComponent, ListItemComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
