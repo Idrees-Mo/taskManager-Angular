015276a408b9598b1d4ef05bd285a15283e8b6a5
Mu Idrees <idris@y7mail.com>
2021-08-25 19:15:29 +0930

    add layout & sytyles

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-const MaterialComponents = [MatToolbarModule, DragDropModule];
+const MaterialComponents = [
+  MatToolbarModule,
+  DragDropModule,
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  DragDropModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
