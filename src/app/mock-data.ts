7f3307ae7340c3dc458c49496458e86cb93e013f
Muhammad Idrees <idris@y7mail.com>
2023-07-10 18:31:28 +1000

    update changes

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -1,12 +1,11 @@
 export const TASKS = [
-  { id: 1, title: 'task 01', selected: false },
-  { id: 2, title: 'task 02', selected: false },
-  { id: 3, title: 'task 03', selected: false },
-  { id: 4, title: 'task 04', selected: false },
+  { id: 1, title: 'Task 04', selected: false },
+  { id: 2, title: 'Task 05', selected: false },
+  { id: 3, title: 'Task 06', selected: false },
 ];
 
 export const LISTS = [
-  { id: 1, title: 'Urgent', tasks: [{ title: 'test-1 task' }] },
-  { id: 2, title: 'High Priority', tasks: [{ title: 'test-2 task' }] },
-  { id: 3, title: 'Low Priority', tasks: [{ title: 'test-3 task' }] },
+  { id: 1, title: 'Urgent', tasks: [{ title: 'Task 01' }] },
+  { id: 2, title: 'High Priority', tasks: [{ title: 'Task 02' }] },
+  { id: 3, title: 'Low Priority', tasks: [{ title: 'Task 07' }] },
 ];
